9612e8a10e5ba8980e133c9539c7bc0bb47ce83b
swapnil1393 <swapnilp1393@gmail.com>
2024-03-27 09:00:54 +0530

    Maven file uploading

---
+++ Company/src/main/java/com/java/sample/Sample.java
@@ -0,0 +1,9 @@
+package com.java.sample;
+
+public class Sample {
+
+	public static void main(String[] args) {
+    System.out.println("i am maven project , going to be store on central repository");
+	}
+
+}
